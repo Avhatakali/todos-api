13a7c99eb5f3b653c47e5985c6b35cf905d0ba36
Avhatakali Makwarela <avhatakali@Avhatakalis-iMac.local>
2019-04-17 11:39:08 +0200

    finish authenticate users

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,14 +3,14 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
 # Define custom error subclasses - rescue catches `StandardErrors`
-  class AuthaticationError < StandardError; end
+  class AuthenticationError < StandardError; end
   class MissingToken < StandardError; end
   class InvalidToken < StandardError; end
 
   included do
     # Define custom handlers
     rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
-    rescue_from ExceptionHandler::AuthaticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
     rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
     rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
 
